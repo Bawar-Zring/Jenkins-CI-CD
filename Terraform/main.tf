615d49aece0ede3e8dde5eff8bff2e2a0bcdc321
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-04-05 11:36:57 +0300

    fixing

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -170,7 +170,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks-node-role.arn
-  subnet_ids      = [aws_subnet.private-AZ1.id, aws_subnet.private-AZ2.id]
+  subnet_ids      = [data.aws_subnet.private1.id, data.aws_subnet.private2.id]
   scaling_config {
     desired_size = 2
     max_size     = 2
